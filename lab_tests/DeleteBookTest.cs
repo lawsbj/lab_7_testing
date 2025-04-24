9176006092599a4ece41d88df6c6a2a81726a85b
Heavy-Scientist <150959212+Heavy-Scientist@users.noreply.github.com>
2025-04-24 13:53:32 -0400

    tests?

---
+++ lab_tests/DeleteBookTest.cs
@@ -0,0 +1,52 @@
+﻿using Laws_Brody_Lab_5.Services;
+
+namespace Lab_5_Testing
+{
+    [DoNotParallelize]
+    [TestClass]
+    public sealed class DeleteBookTest
+    {
+        [TestMethod]
+        public void GoodDeleteBookUsingIdOfRealBook()
+        {
+            //Arrange
+            LibraryService test = new LibraryService();
+            //act 
+            test.AddBook("test", "me", "123");
+
+            test.DeleteBook(1);
+
+            // assert
+
+            Assert.AreEqual(0, test.Books.Count());
+        }
+        [TestMethod]
+        public void BadDeleteBookUsingIdOfNotRealBook()
+        {
+            //Arrange
+            LibraryService test = new LibraryService();
+            //act 
+            test.AddBook("test", "me", "123");
+
+            test.DeleteBook(2);
+
+            // assert
+
+            Assert.AreEqual(1, test.Books.Count());
+        }
+        [TestMethod]
+        public void BadDeleteBookUsingNegativeIdOfNotRealBook()
+        {
+            //Arrange
+            LibraryService test = new LibraryService();
+            //act 
+            test.AddBook("test", "me", "123");
+
+            test.DeleteBook(-1);
+
+            // assert
+
+            Assert.AreEqual(1, test.Books.Count());
+        }
+    }
+}
