30b2eae73f57c3c53c428441515781aaafc980d8
Heavy-Scientist <150959212+Heavy-Scientist@users.noreply.github.com>
2025-04-24 12:57:19 -0400

    dope

---
+++ lab_7/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Laws_Brody_Lab_5.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
