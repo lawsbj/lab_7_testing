44c07be7a024053b5643a61fbc74f72bba8f94d2
Heavy-Scientist <150959212+Heavy-Scientist@users.noreply.github.com>
2025-04-24 13:36:18 -0400

    wdwa

---
+++ Services/ILibraryService.cs
@@ -0,0 +1,31 @@
+﻿using Laws_Brody_Lab_5.Models;
+
+namespace Laws_Brody_Lab_5.Services
+{
+    public interface ILibraryService
+    {
+        public List<User> Users { get; set; }
+
+        public List<Book> Books { get; set; }
+        public Dictionary<User, List<Book>> BorrowedBooks { get; set; }
+
+        // books 
+        List<Book> ReadBooks();
+
+        void AddBook(string title, string author, string isbn);
+        void DeleteBook(int bookId);
+        void EditBook(int bookId, string title, string author, string isbn);
+
+        // users 
+        List<User> ReadUsers();
+        void AddUser(string name, string email);
+        void DeleteUser(int userId);
+        void EditUser(int userId, string name, string email);
+
+        // saving functions
+        void SaveUsersToCSV();
+
+        void SaveBooksToCSV();
+
+    }
+}
